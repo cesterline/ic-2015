64f26b29f35c9ef40b5f914a9cb47a76c7dc97a7
cesterline <chris.esterline@gmail.com>
2015-05-07 10:26:01 -0700

    Adding support for IE
    currently these changes affect ie-8 only

---
+++ js/script-ie.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	/* solve for ie8's lack of support for pseudo selectors */
+	$('#utility > .inline-list > ul > li:last-child').css({ "border-right-width": "1px", "margin-right": "0" });
+	$('nav .btn-group:first-of-type').css({ "background": "none", "padding-left": "0" });
+	$('#legal > ul > li:last-child').css({ "padding-right": "0", "margin-right": "0" });
+
+});
