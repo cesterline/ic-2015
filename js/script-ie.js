39a1d03b9b5463f6efaef2743441397dae781ea1
cesterline <chris.esterline@gmail.com>
2015-05-11 15:44:42 -0700

    header rewrite
    +  to enable more flexible, unified template, approach

--- js/script-ie.js
+++ js/script-ie.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	/* solve for ie8's lack of support for pseudo selectors */
 	$('#utility > .inline-list > ul > li:last-child').css({ "border-right-width": "1px", "margin-right": "0" });
-	$('#channels .btn-group:last-of-type').css({ "background": "none", "padding-left": "0" });
+	$('#channels .btn-group:last-of-type').css({ "background": "none" });
 	$('#legal > ul > li:last-child').css({ "padding-right": "0", "margin-right": "0" });
 
 });
