1f35ae5a3df88998640507e49afb5b073341877b
cesterline <chris.esterline@gmail.com>
2015-05-15 15:05:21 -0700

    Adding Tables/Data-grids
    Additional functionality to be added at a later date

--- js/script-ie.js
+++ js/script-ie.js
@@ -3,4 +3,5 @@ $(document).ready(function() {
 	$('#utility > .inline-list > ul > li:last-child').css({ "border-right-width": "1px", "margin-right": "0" });
 	$('#channels .btn-group:last-of-type').css({ "background": "none" });
 	$('#legal > ul > li:last-child').css({ "padding-right": "0", "margin-right": "0" });
+	$('.table-striped > tbody > tr:nth-of-type(odd)').css({ "background-color": "#f9f9f9" });
 });
