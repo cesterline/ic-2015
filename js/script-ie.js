38e922c9be04542d4d3ebb1614f71c18ca4a164a
cesterline <chris.esterline@gmail.com>
2015-05-20 11:32:16 -0700

    Added Data Grid 'filter' feature
    + also added script-ie.js to IE9
    + improved panel info element padding consistency
    + removed non-used classes

--- js/script-ie.js
+++ js/script-ie.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
 	/* solve for ie8's lack of support for pseudo selectors */
-	$('#utility > .inline-list > ul > li:last-child').css({ "border-right-width": "1px", "margin-right": "0" });
-	$('#channels .btn-group:last-of-type').css({ "background": "none" });
-	$('#legal > ul > li:last-child').css({ "padding-right": "0", "margin-right": "0" });
-	$('.table-striped > tbody > tr:nth-of-type(odd)').css({ "background-color": "#f9f9f9" });
+	$('.lt-ie9 #utility > .inline-list > ul > li:last-child').css({ "border-right-width": "1px", "margin-right": "0" });
+	$('.lt-ie9 #channels .btn-group:last-of-type').css({ "background": "none" });
+	$('.lt-ie9 #legal > ul > li:last-child').css({ "padding-right": "0", "margin-right": "0" });
+	$('.lt-ie9 .table-striped > tbody > tr:nth-of-type(odd)').css({ "background-color": "#f9f9f9" });
+
+	$('.no-label[placeholder]').focus(function() {
+	  var input = $(this);
+	  if (input.val() == input.attr('placeholder')) {
+	    input.val('');
+	    input.removeClass('placeholder');
+	  }
+	}).blur(function() {
+	  var input = $(this);
+	  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+	    input.addClass('placeholder');
+	    input.val(input.attr('placeholder'));
+	  }
+	}).blur().parents('form').submit(function() {
+	  $(this).find('[placeholder]').each(function() {
+	    var input = $(this);
+	    if (input.val() == input.attr('placeholder')) {
+	      input.val('');
+	    }
+	  })
+	});
+
 });
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-
+	/* setting default values of disabled fields for purpose of demo only */
+	$("#df-1").val("555-555-1212");
+	$("#df-2").val("email@domain.com");
 });
